2fef3a1a171721fa2c0f15e0dafa5daddff6737c
lihuan <1968763315@qq.com>
2019-12-01 16:26:35 +0800

    12.1

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -26,6 +26,6 @@ export default {
 .goods {
   display: flex;
   flex-wrap: wrap; 
-  justify-content: space-around;
+  justify-content: space-evenly;
 }
 </style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,12 +3,115 @@
     <nav-bar class="home-nav">
       <div slot="center">购物商城</div>
     </nav-bar>
+    <scroll class="content">
     <home-swiper :cbanners="banners"></home-swiper>
     <recommend :crecommends="recommends" />
     <feature />
     <tab-control :titles="['流行', '新款', '精选']" class="tab-control" @tabClick="tabClick"/>
-    <goods-list :goods="goods[currentType].list"/>
-    
+    <goods-list :goods="showGoods"/>
+    </scroll>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+      <li>列表51</li>
+      <li>列表52</li>
+      <li>列表53</li>
+      <li>列表54</li>
+      <li>列表55</li>
+      <li>列表56</li>
+      <li>列表57</li>
+      <li>列表58</li>
+      <li>列表59</li>
+      <li>列表60</li>
+      <li>列表61</li>
+      <li>列表62</li>
+      <li>列表63</li>
+      <li>列表64</li>
+      <li>列表65</li>
+      <li>列表66</li>
+      <li>列表67</li>
+      <li>列表68</li>
+      <li>列表69</li>
+      <li>列表70</li>
+      <li>列表71</li>
+      <li>列表72</li>
+      <li>列表73</li>
+      <li>列表74</li>
+      <li>列表75</li>
+      <li>列表76</li>
+      <li>列表77</li>
+      <li>列表78</li>
+      <li>列表79</li>
+      <li>列表80</li>
+      <li>列表81</li>
+      <li>列表82</li>
+      <li>列表83</li>
+      <li>列表84</li>
+      <li>列表85</li>
+      <li>列表86</li>
+      <li>列表87</li>
+      <li>列表88</li>
+      <li>列表89</li>
+      <li>列表90</li>
+      <li>列表91</li>
+      <li>列表92</li>
+      <li>列表93</li>
+      <li>列表94</li>
+      <li>列表95</li>
+      <li>列表96</li>
+      <li>列表97</li>
+      <li>列表98</li>
+      <li>列表99</li>
+      <li>列表100</li>
+    </ul>
   </div>
 </template>
 
@@ -19,7 +122,8 @@ import Feature from "./child/Feature";
 
 import NavBar from 'components/common/navbar/NavBar';
 import TabControl from 'components/content/tabcontrol/TabControl';
-import GoodsList from 'components/content/goods/GoodsList'
+import GoodsList from 'components/content/goods/GoodsList';
+import Scroll from 'components/common/scroll/Scroll'
 
 import { getHomeMultidata, getHomeGoods } from "network/home";
 
@@ -37,13 +141,19 @@ export default {
       currentType: 'pop'
     }
   },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
+    }
+  },
   components: {
     NavBar,
     HomeSwiper,
     Recommend,
     Feature,
     TabControl,
-    GoodsList
+    GoodsList,
+    Scroll
   },
   created() {
     //1请求轮播图和推荐数据
@@ -93,6 +203,7 @@ export default {
 <style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
 }
 .home-nav {
   background-color: var(--color-tint);
@@ -108,5 +219,10 @@ export default {
   top: 44px;
   background-color: #fff;
   z-index: 9;
+}
+.content {
+  height: calc(100%-98px);
+  overflow: hidden;
+  
 }
 </style>
