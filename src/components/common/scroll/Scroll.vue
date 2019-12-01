5a9df2458efcfdcc6875a6729c3e15c55e2ec6d1
lihuan <1968763315@qq.com>
2019-12-02 01:08:09 +0800

    12.2

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -10,15 +10,43 @@
 import BScroll from 'better-scroll'
 export default {
   name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    }
+  },
  data() {
    return {
      scroll: null
    }
  },
  mounted() {
+   //创建BScroll对象
    this.scroll = new BScroll(this.$refs.wrapper,{
+     click: true,
+     probeType: this.probeType,
+     pullUpLoad: this.pullUpLoad
+   })
+   //监听滚动位置
+   this.scroll.on('scroll',position =>{
+     this.$emit('scroll',position)
      
    })
+   //监听上拉事件
+   this.scroll.on('pullingUp',() =>{
+     this.$emit('pullingUp')
+     
+   })
+ },
+ methods: {
+   scrollTo(x, y, time=500) {
+     this.scroll.scrollTo(x, y, time)
+   }
  }
 }
 </script>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,115 +3,15 @@
     <nav-bar class="home-nav">
       <div slot="center">购物商城</div>
     </nav-bar>
-    <scroll class="content">
+    <scroll class="content" ref="scroll" 
+    :probe-type="3" @scroll="contentScroll" :pull-up-load="true" @pullingUp="loadMore">
     <home-swiper :cbanners="banners"></home-swiper>
     <recommend :crecommends="recommends" />
     <feature />
     <tab-control :titles="['流行', '新款', '精选']" class="tab-control" @tabClick="tabClick"/>
     <goods-list :goods="showGoods"/>
     </scroll>
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-      <li>列表51</li>
-      <li>列表52</li>
-      <li>列表53</li>
-      <li>列表54</li>
-      <li>列表55</li>
-      <li>列表56</li>
-      <li>列表57</li>
-      <li>列表58</li>
-      <li>列表59</li>
-      <li>列表60</li>
-      <li>列表61</li>
-      <li>列表62</li>
-      <li>列表63</li>
-      <li>列表64</li>
-      <li>列表65</li>
-      <li>列表66</li>
-      <li>列表67</li>
-      <li>列表68</li>
-      <li>列表69</li>
-      <li>列表70</li>
-      <li>列表71</li>
-      <li>列表72</li>
-      <li>列表73</li>
-      <li>列表74</li>
-      <li>列表75</li>
-      <li>列表76</li>
-      <li>列表77</li>
-      <li>列表78</li>
-      <li>列表79</li>
-      <li>列表80</li>
-      <li>列表81</li>
-      <li>列表82</li>
-      <li>列表83</li>
-      <li>列表84</li>
-      <li>列表85</li>
-      <li>列表86</li>
-      <li>列表87</li>
-      <li>列表88</li>
-      <li>列表89</li>
-      <li>列表90</li>
-      <li>列表91</li>
-      <li>列表92</li>
-      <li>列表93</li>
-      <li>列表94</li>
-      <li>列表95</li>
-      <li>列表96</li>
-      <li>列表97</li>
-      <li>列表98</li>
-      <li>列表99</li>
-      <li>列表100</li>
-    </ul>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
@@ -123,7 +23,8 @@ import Feature from "./child/Feature";
 import NavBar from 'components/common/navbar/NavBar';
 import TabControl from 'components/content/tabcontrol/TabControl';
 import GoodsList from 'components/content/goods/GoodsList';
-import Scroll from 'components/common/scroll/Scroll'
+import Scroll from 'components/common/scroll/Scroll';
+import BackTop from 'components/content/backtop/BackTop';
 
 import { getHomeMultidata, getHomeGoods } from "network/home";
 
@@ -138,7 +39,8 @@ export default {
         'new': { page: 0, list: [] },
         'sell': { page: 0, list: [] }
       },
-      currentType: 'pop'
+      currentType: 'pop',
+      isShowBackTop: false
     }
   },
   computed: {
@@ -153,7 +55,8 @@ export default {
     Feature,
     TabControl,
     GoodsList,
-    Scroll
+    Scroll,
+    BackTop
   },
   created() {
     //1请求轮播图和推荐数据
@@ -180,6 +83,7 @@ export default {
        getHomeGoods(type, page).then(res => {
          this.goods[type].list.push(...res.data.list)
          this.goods[type].page += 1
+         this.$refs.scroll.scroll.finishPullUp()
     })
     },
     //事件监听相关
@@ -195,6 +99,17 @@ export default {
           this.currentType = 'sell' 
           break   
       }
+    },
+    backClick() {
+      this.$refs.scroll.scrollTo(0,0) 
+    },
+    contentScroll(position) {
+      this.isShowBackTop = (- position.y)> 1000
+      
+    },
+    loadMore() {
+      this.getHomeGoods(this.currentType)
+      
     }
   }
 }
@@ -221,7 +136,7 @@ export default {
   z-index: 9;
 }
 .content {
-  height: calc(100%-98px);
+  height: calc(100% - 49px);
   overflow: hidden;
   
 }
