bda5592174fc457a851d2489f27807a664fd8f6b
lihuan <1968763315@qq.com>
2019-12-06 23:07:06 +0800

    12.6

--- src/network/detail.js
+++ src/network/detail.js
@@ -27,13 +27,14 @@ export class Goods {
   }
 }
 export class Shop {
-	constructor(shopInfo) {
-		this.logo = shopInfo.shopLogo;
-		this.name = shopInfo.name;
-		this.fans = shopInfo.cFans;
-		this.sells = shopInfo.cSells;
-		this.score = shopInfo.score;
-		this.goodsCount = shopInfo.cGoods
+      constructor(shopInfo) {
+
+        this.logo = shopInfo.shopLogo;
+        this.name = shopInfo.name;
+        this.fans = shopInfo.cFans;
+        this.sells = shopInfo.cSells;
+        this.score = shopInfo.score;
+        this.goodsCount = shopInfo.cGoods
 	}
 }
 export class GoodsParam {
