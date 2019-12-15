c32096fc0eee5986dfda59d17bd48262904f58f0
lihuan <1968763315@qq.com>
2019-12-15 20:33:38 +0800

    12.15

--- src/network/detail.js
+++ src/network/detail.js
@@ -39,7 +39,7 @@ export class Shop {
 }
 export class GoodsParam {
 	constructor(info, rule) {
-		// 注: images可能没有值(某些商品有值, 某些没有值)
+		// images可能没有值(某些商品有值, 某些没有值)
 		this.image = info.images ? info.images[0] : '';
 		this.infos = info.set;
 		this.sizes = rule.tables;
